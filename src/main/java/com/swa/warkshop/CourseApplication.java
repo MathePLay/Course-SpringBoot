5bdaf622fb713f3ae6af3ea6c110559ed8f2e10f
Matheus Doerzbacher <matheus.doer@gmail.com>
2022-08-11 10:58:40 -0300

    Project created

---
+++ src/main/java/com/swa/warkshop/CourseApplication.java
@@ -0,0 +1,13 @@
+package com.swa.warkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+	}
+
+}
